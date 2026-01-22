a42ea6e6fd123424b23ca9ddeef3111eb103fc35
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-01-22 15:21:46 +0500

    add python basic functions practice

---
+++ python/data_types.py
@@ -0,0 +1,19 @@
+# Integer
+age = 18
+print("Age:", age)
+print("Data type of age:", type(age))
+
+# Float
+height = 5.9
+print("\nHeight:", height)
+print("Data type of height:", type(height))
+
+# String
+name = "Zikashi"
+print("\nName:", name)
+print("Data type of name:", type(name))
+
+# Boolean
+is_student = True
+print("\nIs Student:", is_student)
+print("Data type of is_student:", type(is_student))
