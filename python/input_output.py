ca7ec7b46f8e383d4d9eca8cc978cd5d20bcf135
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-01-23 17:19:41 +0500

    add python basic functions practice

---
+++ python/input_output.py
@@ -0,0 +1,8 @@
+# input of data types
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height (in meters): "))
+# print/outputs
+print("Your name is:", name)
+print("Your age is:", age)
+print("Your height is:", height)
