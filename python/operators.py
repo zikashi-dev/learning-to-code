fe01c1a12a6016cdade8085838d5d1cb86f5f260
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-01-24 21:14:31 +0500

    add python basic functions practice

---
+++ python/operators.py
@@ -0,0 +1,10 @@
+# Taking input from user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Arithmetic operators
+print("\nAddition (+):", a + b)
+print("Subtraction (-):", a - b)
+print("Multiplication (*):", a * b)
+print("Division (/):", a / b)
+print("Modulus (%):", a % b)
