569cbf619222652d674cebcc2639651f30899250
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-01-25 19:53:08 +0500

    add python basic functions practice

---
+++ python/conditions.py
@@ -0,0 +1,14 @@
+# Taking input from user
+marks = int(input("Enter your marks: "))
+
+# Conditions
+if marks >= 80:
+    print("Grade: A+")
+elif marks >= 70:
+    print("Grade: A")
+elif marks >= 60:
+    print("Grade: B")
+elif marks >= 50:
+    print("Grade: C")
+else:
+    print("Fail")
