9ad5df20010ac39092fbcb4160e6567b1b5600f8
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-01-26 20:57:20 +0500

    add python basic functions practice

--- python/functions.py
+++ python/functions.py
@@ -1,6 +1,13 @@
 # Function practice
+def greet_user(name):
+    print("Hello,", name)
+greet_user("Zikashi")
 
-def greet(name):
-    return f"Hello, {name}!"
-
-print(greet("Zikashi"))
+# Function returning multiple values
+def calculate(a, b):
+    sum_value = a + b
+    product = a * b
+    return sum_value, product
+s, p = calculate(4, 6)
+print("Sum:", s)
+print("Product:", p)
