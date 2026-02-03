7f241728f02f61c964b8c9d205bb9f4e6ed84922
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-02-03 21:15:15 +0500

    add python basic functions practice

--- python/todo_cli.py
+++ python/todo_cli.py
@@ -10,26 +10,26 @@ while True:
     if choice == "1":
         task = input("Enter a new task: ")
         tasks.append(task)
-        print("✅ Task added!")
+        print(" Task added!")
     elif choice == "2":
         if not tasks:
-            print("📭 No tasks found.")
+            print(" No tasks found.")
         else:
             print("\nYour Tasks:")
             for i in range(len(tasks)):
                 print(f"{i + 1}. {tasks[i]}")
     elif choice == "3":
         if not tasks:
-            print("📭 No tasks to delete.")
+            print(" No tasks to delete.")
         else:
             task_num = int(input("Enter task number to delete: "))
             if 1 <= task_num <= len(tasks):
                 removed = tasks.pop(task_num - 1)
-                print(f"🗑️ Removed: {removed}")
+                print(f" Removed: {removed}")
             else:
-                print("❌ Invalid task number.")
+                print(" Invalid task number.")
     elif choice == "4":
-        print("👋 Exiting To-Do App. Bye!")
+        print(" Exiting To-Do App. Bye!")
         break
     else:
-        print("❌ Invalid choice. Try again.")
+        print(" Invalid choice. Try again.")
