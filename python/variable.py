1d85d8f7862ee57999facb087a5e87258e77da5e
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-07-14 23:17:46 +0500

    update variable.py

--- python/variable.py
+++ python/variable.py
@@ -7,3 +7,7 @@ learning = True
 print(name)
 print(age)
 print(learning)
+print("Zaryab" , "Ali" , "Musfira" , "Malik" , "Eleven" , sep='/' )
+# this line 10 print keyword will print each value with slash'/' as we used sep keyword
+print("Hello", end = '')
+print("world")
