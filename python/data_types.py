644f2538869f9b53a276bbb0d59c34ccb6fc886c
Zikashi <146316243+zikashi-dev@users.noreply.github.com>
2026-07-14 23:36:48 +0500

    Expand data types examples in data_types.py
    Added examples for complex numbers, lists, tuples, sets, and dictionaries.

--- python/data_types.py
+++ python/data_types.py
@@ -2,11 +2,17 @@
 age = 18
 print("Age:", age)
 print("Data type of age:", type(age))
+print(1e309)
 
 # Float
 height = 5.9
 print("\nHeight:", height)
 print("Data type of height:", type(height))
+print(1.7e309)
+
+#complex
+print(4+5j)
+(4+5j)
 
 # String
 name = "Zikashi"
@@ -17,3 +23,12 @@ print("Data type of name:", type(name))
 is_student = True
 print("\nIs Student:", is_student)
 print("Data type of is_student:", type(is_student))
+
+#list
+print([1,2,3,4,5,6])
+#tuple
+print((1,2,3,4,5))
+#sets
+print({1,2,3,4,5,6})
+#dict
+print({"Pakistan":"Chine","Country code":91,"Turkiye":"Japan"})
